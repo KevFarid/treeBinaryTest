bdf9835afcabd051e608294df6884e83823357fe
Kevin Farid <kpelaez75@gmail.com>
2020-01-09 04:01:10 -0500

    Mejora al menu inicial

--- ArbolBinario/Program.cs
+++ ArbolBinario/Program.cs
@@ -18,7 +18,7 @@ namespace ArbolBinario
             do
             {
                 Console.WriteLine("\n================================");
-                Console.WriteLine("1.insertar nodo \n2.Imprimir \n3.Lowest Common Ancestor \n4.Salir");
+                Console.WriteLine("1.insertar arbol \n2.Imprimir \n3.Lowest Common Ancestor \n4.Salir");
 
                 text = Console.ReadLine();
 
@@ -26,16 +26,30 @@ namespace ArbolBinario
                 switch (text)
                 {
                     case "1":
-                        Console.WriteLine("\n==========================\nInserte el numero para el nodo");
-                        num = Convert.ToInt32(Console.ReadLine());
-                        tree = arbolBinario.insertNodo(tree, num);
+                        Console.WriteLine("\n==========================");
+                        Console.WriteLine("Inserte cada nodo escribiendo el numero y precionando la tecla 'Enter'");
+                        Console.WriteLine("Cuando termine escriba: end");
+
+                        string numText;
+                        
+                        do {
+                            numText = Console.ReadLine().ToLower();
+
+                            if( numText != "end")
+                            {
+                            num = Convert.ToInt32(numText);
+                            tree = arbolBinario.insertNodo(tree, num);
+                            }
+
+                        } while(numText != "end");
                         break;
 
                     case "2":
-                        Console.WriteLine("\n==========================\nNodo imprimido\n\n");
+                        Console.WriteLine("\n==========================\nArbol imprimido \nPrecione la tecla 'Enter' para salir \n");
                         arbolBinario.showTree(tree, 0);
                         Console.ReadKey();
                         break;
+
                     case "3":
                         Console.WriteLine("\n==========================\nLowest Common Ancestor");
 
@@ -43,21 +57,24 @@ namespace ArbolBinario
                         int nodo2;
                         Nodo nodoAncestor;
 
-                        Console.WriteLine("Escriba el par de nodo que quiera buscar");
+                        Console.WriteLine("Escriba el par de nodo que quiere buscar\n");
                         Console.WriteLine("Escriba el nodo 1");
                         nodo1 = Convert.ToInt32( Console.ReadLine() );
                         
-                        Console.WriteLine("Escriba el nodo 2");
+                        Console.WriteLine("\nEscriba el nodo 2");
                         nodo2 = Convert.ToInt32( Console.ReadLine() );
 
                         nodoAncestor = arbolBinario.LCA(tree, arbolBinario.findNodo(tree, nodo1), arbolBinario.findNodo(tree, nodo2));
 
-                        Console.WriteLine( "Ancestor{" + nodo1 + ", " + nodo2 + "} = " + nodoAncestor.num);
+                        Console.WriteLine( "\nAncestor{" + nodo1 + "," + nodo2 + "} = " + nodoAncestor.num);
+                        
+                        Console.WriteLine("\nPrecione la tecla 'Enter' para salir");
                         Console.ReadKey();
                         break;
 
                     default:
                         break;
+
                 }
 
             } while (text != "4");
